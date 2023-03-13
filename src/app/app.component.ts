af66a2227b12cf5cceee094202d874b25d465069
Jeff Campbell <86787426+citizenjett@users.noreply.github.com>
2023-03-13 13:31:16 -0400

    add new component sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'This is My First App';
+
+  // add some additional properties..
+  firstName = 'Bill';
+  lastName = 'Smith';
 }
